57d85b40ff6a451d2ba717810626a15c6f008241
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-07-30 21:37:30 -0500

    added nesscary files

--- store/models.py
+++ store/models.py
@@ -20,13 +20,13 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    @property
-    def imageURL(self):
-        try:
-            url = self.image.url
-        except:
-            url = ''
-        return url
+    # @property
+    # def imageURL(self):
+    #     try:
+    #         url = self.image.url
+    #     except:
+    #         url = ''
+    #     return url
 
 
 class Order(models.Model):
