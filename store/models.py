8d6bc70a994e64dfdcd9b0726fc903298e2f9011
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-08-02 12:08:58 -0500

    starting tutorial over

--- store/models.py
+++ store/models.py
@@ -32,7 +32,7 @@ class Order(models.Model):
     
     
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, one_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
