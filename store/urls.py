a1bf5f8ad7aada7a8e5cb485414a9497221077cc
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-08-02 11:30:14 -0500

    starting tutorial over

--- store/urls.py
+++ store/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name="store"),
-    # path('cart/', views.cart, name="cart"),
-    # path('checkout/', views.checkout, name="checkout"),
-    # path('products/', views.products, name="products"),
-    # path('about/', views.about, name="about"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('products/', views.products, name="products"),
+    path('about/', views.about, name="about"),
     # path('update_item/', views.updateItem, name="update_item"),
     # path('process_order/', views.processOrder, name="processOrder"),
 ]
--- store/views.py
+++ store/views.py
@@ -4,3 +4,20 @@ def store(request):
     context = {}
     return render(request, 'store/store.html', context)
 
+def about(request):
+    context = {}
+    return render(request, 'store/about.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+    
+def products(request):
+    context = {}
+    return render(request, 'store/products.html', context)
+
