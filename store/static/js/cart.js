f229047046d18ddd76240078690b1103486b7149
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-08-02 13:58:47 -0500

    starting tutorial over

--- store/static/js/cart.js
+++ store/static/js/cart.js
@@ -6,5 +6,13 @@ for (i = 0; i < updateBtns.length; i++) {
         var action = this.dataset.action
 
         console.log('productId: ', productId, 'action: ', action)
+
+        console.log('USER: ', user)
+
+        if(user === 'AnonymousUser'){
+            console.log('Not logged in')
+        }else{
+            console.log('User is logged in, sending data...')
+        }
     })
 }
